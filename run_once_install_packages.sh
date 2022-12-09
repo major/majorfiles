5b140d3127657ffc79427f0116d68615288e2c6e
Major Hayden <major@mhtx.net>
2022-12-09 16:07:14 -0600

    Add zulu JDK + mesa swap

--- run_once_install_packages.sh
+++ run_once_install_packages.sh
@@ -32,4 +32,18 @@ package_del=(
 sudo dnf -y install $(IFS=' '; echo "${package_add[*]}")
 sudo dnf -y remove $(IFS=' '; echo "${package_del[*]}")
 
+# Enable downloading of updates.
 sudo systemctl enable --now dnf-automatic-download.timer
+
+# Prepare Zulu JDK for Thinkorswim
+sudo dnf -y install https://cdn.azul.com/zulu/bin/zulu-repo-1.0.0-1.noarch.rpm
+sudo dnf -y install zulu11-jdk
+
+# Set up RPM Fusion
+sudo dnf install \
+    https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+# Switch to the mesa with AMD acceleration
+sudo dnf swap mesa-va-drivers mesa-va-drivers-freeworld
+sudo dnf swap mesa-vdpau-drivers mesa-vdpau-drivers-freeworld
