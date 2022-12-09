4b1a6bb3857a53349e1ffd65605d9b64cff9e84d
Major Hayden <major@mhtx.net>
2022-12-09 06:59:58 -0600

    Add run once script

---
+++ run_once_install_packages.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Install packages.
+
+source /etc/os-release
+
+sudo rpm-ostree install -y \
+    alacritty blueman brightnessctl dmenu dunst feh i3 i3-config-fedora i3lock \
+    i3status light lxappearance maim network-manager-applet pasystray pavucontrol \
+    picom playerctl rofi rofimoji Thunar xclip xinput vorbis-tools
+
+# Prepare the Zulu 11 JDK for Thinkorswim
+sudo rpm-ostree install -y https://cdn.azul.com/zulu/bin/zulu-repo-1.0.0-1.noarch.rpm
+sudo rpm-ostree install -y zulu11-jdk
+
+# Install regular OpenJDK 11
+sudo rpm-ostree install -y java-11-openjdk
+
+# Create Fedora toolbox matching Fedora version
+toolbox -y create --distro fedora --release f${VERSION_ID}
+
+# Create Fedora rawhide toolbox
+toolbox -y create --distro fedora --release rawhide
+
